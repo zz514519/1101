7de7435718a97615d86a1db886e66ac9b9b03b6a
zz514519 <1475430017@qq.com>
2019-11-04 22:30:17 +0800

    练习

---
+++ ListDemo.java
@@ -0,0 +1,10 @@
+/**
+ * Author : Zhang Zhe
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+        DemoList demoList = new DemoList();
+        demoList.addfirst(23);
+        demoList.display();
+    }
+}
